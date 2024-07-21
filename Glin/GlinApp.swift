1fef3e903fd1368a75094297b62eaf742e6c295f
Niral Patel <patnir41@stanford.edu>
2024-07-21 16:23:04 -0400

    Initial Commit

---
+++ Glin/GlinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlinApp.swift
+//  Glin
+//
+//  Created by Niral Patel on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GlinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
